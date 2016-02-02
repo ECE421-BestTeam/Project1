12431cb412e57f5e9960b1d91e6bdc9946fc1c3d
cho8 <c.ho784@gmail.com>
2016-02-01 19:47:59 -0700

    Fixed issue with invariants

--- test/sparse_matrix_validation_test.rb
+++ test/sparse_matrix_validation_test.rb
@@ -13,17 +13,15 @@ class TestSparseMatrix < Test::Unit::TestCase
     @m4 = SparseMatrix[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
     @n1 = SparseMatrix[[2,3,4], [5,6,7]]
     
-    check_invariant
+    checkInvariant(@m1, @m2, @m3, @m4, @n1)
   end
   
   def teardown
-    check_invariant
+    checkInvariant(@m1, @m2, @m3, @m4, @n1)
   end
   
-  def check_invariant
-    iv = self.instance_variables
-    iv.each do |i|
-      m = self.instance_variable_get(i)
+  def checkInvariant(*args)
+    args.each do |m|
       assert(m.row_count >= 0)
       assert(m.column_count >= 0)
       assert_kind_of(Matrix, m)
