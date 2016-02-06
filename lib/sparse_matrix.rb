8c5b79d0c0f4b015ce8a347a107113496650ed45
lindsayl <livojevi@ualberta.ca>
2016-02-05 19:01:51 -0700

    started inverse

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -16,6 +16,57 @@ class SparseMatrix < Matrix
   attr_accessor :column_count
   attr_accessor :row_count
   
+  
+  # Is just (matrix * thing.inverse) so it should be faster just by using our inverse+determinant functions
+#  def /(thing)
+#    
+#  end
+  
+  def inverse
+    det = determinant
+    Matrix.Raise ErrNotRegular if det == 0
+    
+    a = @rows  #Make a deep copy!
+
+    (0..(@column_count - 1)).each do |k|
+      i = k
+      akk = a[k, k].abs
+      ((k + 1)..(@column_count - 1)).each do |j|
+        v = a[j, k].abs
+        if v > akk
+          i = j
+          akk = v
+        end
+      end
+      Matrix.Raise ErrNotRegular if akk == 0
+      if i != k
+        a[i], a[k] = a[k], a[i]
+        @rows[i], @rows[k] = @rows[k], @rows[i]
+      end
+      akk = a[k, k]
+
+      (0..(@column_count - 1)).each do |ii|
+        next if ii == k
+        q = a[ii, k].quo(akk)
+        a[ii, k] = 0
+
+        (k + 1).upto(last) do |j|
+          a[ii, j] -= a[k, j] * q
+        end
+        (0..(@column_count - 1)).each do |j|
+          @rows[ii][j] -= @rows[k][j] * q
+        end
+      end
+
+      ((k + 1)..(@column_count - 1)).each do |j|
+        a[k][j] = a[k][j].quo(akk)
+      end
+      (0..(@column_count - 1)).each do |j|
+        @rows[k][j] = @rows[k][j].quo(akk)
+      end
+    end
+  end
+  
   def determinant()
     if (@column_count != @row_count)
       SparseMatrix.Raise ErrDimensionMismatch
