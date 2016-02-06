f06ef9002f90b13c90ab18d4b2936da9d99231f1
cho8 <c.ho784@gmail.com>
2016-02-05 19:34:25 -0700

    added sparsehash and tests

--- lib/sparse_hash.rb
+++ lib/sparse_hash.rb
@@ -1,30 +1,43 @@
 class SparseHash < Hash
   attr_reader :size
 
-  def initialize (rows, cols, default = nil, &block)
-    @rows = rows
-    @cols = cols
+  def initialize (size, default = nil, &block)
+    @size = size
     @block = block || Proc.new { default }
     
-    super() { |h,r| r.between?(0, @rows-1) ? @block.call(h,r) : nil}
+    super() { |h,k| @block.call(h,k)}
+  end
+  
+  def hashsize
+    self.length
   end
 
-  def get(row, col)
-    self[row][col].nil? && block_given? ? yield(i) : self[i]
+  def [](i)
+    return !has_key?(i) && check_bound(i) ? 0 :
+    super(i).nil? && block_given? ? yield(i) : super(i)
   end
+  alias get []
+  
+  def []=(i,v)
+    super(i,v) unless v==0
+  end
+  alias set []=
   
   def each
-    return to_enum :each unless block_given?
-    (0...size).each do |i|
+    @size.times do |i|
       yield self[i]
     end
   end
   
-  def map
-    return to_enum :each unless block_given?
-    (0..size).map do |i|
-      yield self[i]
+  def each_sparse
+    self.each_pair do |i,v|
+      yield (v)
     end
+  end
+  
+  
+  def check_bound(i)
+    return i.between?(0, @size-1)
   end
 
 end # end of SparseHash
