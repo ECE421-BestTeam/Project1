888d131576af2bf8fc47eccd2394cc52a91d9ba2
lindsayl <livojevi@ualberta.ca>
2016-02-09 16:07:59 -0700

    fixed up main

--- project1/assignment1_main.rb
+++ project1/assignment1_main.rb
@@ -2,22 +2,17 @@
 # Lindsay Livojevic
 # Jonathan Ohman
 
-require './lib/matrix_factory'
-require './lib/sparse_matrix'
-
-puts "MatrixFactory.rows(Matrix, [[1,2],[3,4]]): #{MatrixFactory.rows(Matrix, [[1,2],[3,4]])}"
-puts "MatrixFactory.rows(SparseMatrix, [[1,2],[3,4]]): #{MatrixFactory.rows(SparseMatrix, [[1,2],[3,4]])}"
-puts "SparseMatrix[[1,2],[3,4]]: #{SparseMatrix[[1,2],[3,4]]}"
-
-require_relative ‘sparse_matrix’
+require_relative './lib/sparse-matrix'
+require_relative './lib/matrix-factory'
 
 # How to create matrices
 denseMatrix = MatrixFactory.rows(Matrix, [[1,0,3],[2,2,0],[0,0,0]])
-sparseMatrix1 = MatrixFactory.rows(SparseMatrix, [[1,0,3],[2,2,0],[0,0,0]])
-sparseMatrix2 = MatrixFactory.build(3,3)  do |row, col| 
-row==col ? row+col  : 0
+sparseMatrix1 = MatrixFactory.rows(SparseMatrix, [[1,0,3],[2,2,0],[0,0,3]])
+sparseMatrix2 = MatrixFactory.build(SparseMatrix, 3,3)  do |row, col| 
+  row == col ? row + col : 0
 end
-zeroMatrix MatrixFactory.zero(3)
+zeroMatrix = MatrixFactory.zero(SparseMatrix, 3)
+
 
 # Operations
 
@@ -25,11 +20,13 @@ addResult = sparseMatrix1 + sparseMatrix2
 
 subResult = sparseMatrix1 - sparseMatrix2
 
-mulResult = sparseMatrix1*sparseMatrix2
+mulResult = sparseMatrix1 * sparseMatrix2
 
-divResult = sparseMatrix1/sparseMatrix2
+divResult = sparseMatrix2 / sparseMatrix1
 
-sparseDeterm = sparseMatrix1.det
+sparseDeterm = sparseMatrix1.determinant
+
+sparseInverse = sparseMatrix1.inverse
 
 zeroTest1 = sparseMatrix1.zero?
 
