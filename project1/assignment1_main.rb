c59f2aba76aade9341b8f2ef414c355bc16128dd
lindsayl <livojevi@ualberta.ca>
2016-02-09 13:34:11 -0700

    Started some stuff in main....

--- project1/assignment1_main.rb
+++ project1/assignment1_main.rb
@@ -1,3 +1,10 @@
 # Christina Ho
 # Lindsay Livojevic
 # Jonathan Ohman
+
+require './lib/matrix_factory'
+require './lib/sparse_matrix'
+
+puts "MatrixFactory.rows(Matrix, [[1,2],[3,4]]): #{MatrixFactory.rows(Matrix, [[1,2],[3,4]])}"
+puts "MatrixFactory.rows(SparseMatrix, [[1,2],[3,4]]): #{MatrixFactory.rows(SparseMatrix, [[1,2],[3,4]])}"
+puts "SparseMatrix[[1,2],[3,4]]: #{SparseMatrix[[1,2],[3,4]]}"
