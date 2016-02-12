28daa719e6697554070b40630823540a7b4577f7
lindsayl <livojevi@ualberta.ca>
2016-02-11 18:15:00 -0700

    Just putting some files in proj2

---
+++ project2/test/some_test.rb
@@ -0,0 +1,19 @@
+# Tests that the factory produces the correct type of matrix
+
+require 'test/unit'
+require_relative '../lib/something'
+
+class SomeTest < Test::Unit::TestCase
+  
+  def setup
+    # do nothing
+  end
+
+  def teardown
+    # do nothing
+  end
+  
+  def test_something
+  end
+  
+end
