6ebf35e7465d822c16fcb3a6028a137b29a71315
cho8 <c.ho784@gmail.com>
2016-02-28 01:01:58 -0700

    forgot to add the benchmarks

---
+++ project3/test/merge-sort-benchmarks.rb
@@ -0,0 +1,56 @@
+require 'test/unit'
+require'../lib/merge-sort'
+require 'benchmark'
+
+class MergeSortTest < Test::Unit::TestCase
+  include MergeSort
+  
+  def setup
+    @a100 = Array.new(100) { rand(100) }
+    @a1000 = Array.new(1000) { rand(1000) }
+    @a10000 = Array.new(10000) { rand(10000) }
+    @a100000 = Array.new(100000) { rand(100000) }
+    @timeout = 0
+
+  end
+  
+  def teardown
+  end
+  
+  def test_benchmarks
+    
+      Benchmark.bm  do |x|
+        x.report("100  builtin:") {
+          @a100.sort
+        }
+        x.report("100   pmerge:") {
+          sortInPlace(@a100, @timeout)
+        }
+      end
+      
+      Benchmark.bm  do |x|
+        x.report("1000  builtin:") {
+          @a1000.sort
+        }
+        x.report("1000   pmerge:") {
+          sortInPlace(@a1000, @timeout)
+        }
+      end
+      
+      Benchmark.bm  do |x|
+        x.report("10000 builtin:") {
+          @a10000.sort
+        }
+        x.report("10000  pmerge:") {
+          sortInPlace(@a10000, @timeout)
+        }
+      end
+
+  end
+  
+  def test_benchmarks_with_timeout
+  
+  end
+end
+  
+  
