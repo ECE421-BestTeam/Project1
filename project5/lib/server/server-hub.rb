075b1a949627f2c16eb59d26c4d13a44c73c7feb
lindsayl <livojevi@ualberta.ca>
2016-03-21 21:25:44 -0600

    trying some server stuff

--- project5/lib/server/server-hub.rb
+++ project5/lib/server/server-hub.rb
@@ -1,3 +1,4 @@
+require 'socket'
 require_relative './server-game'
 require_relative './database/database'
 
@@ -16,25 +17,64 @@ class ServerHub
     end
     
     @db = Database.new
+    @checkOutSemaphore = Mutex.new 
+    
+    @continue = true
+    @server = TCPServer.open(port)
     
   end
   
-  def connectToGameServer(gameId)
-    # check if game is currently checked out to a server
-    game = @db.get(:games, gameId)
-    serverAddress = game[:checkedOutTo]
-    
-    # redirect here
-    return serverAddress if serverAddress
-    
-    #else check it out to a new server
-    address = selectServer(@gameServers)
-    game[:checkedOutTo] = address
-    @db.update(:games, game)
+  def loop
+    while @continue
+      th = Thread.new(@server.accept) do |client|
+        req = client.gets.chomp
+        res = "invalid"
+        case req
+          when "menu"
+            res = connectToMenuServer
+          when "newGame"
+            res = connectToNewGameServer
+          when "existingGame"
+            res = connectToExistingGameServer(client)
+        end
+        client.puts(Time.now.ctime) # Send the time to the client
+        client.puts "Closing the connection. Bye!"
+        client.close                # Disconnect from the client
+      end
+    end
   end
   
   def connectToMenuServer
     return selectServer(@menuServers)
+  end
+  
+  def connectToNewGameServer
+    return selectServer(@gameServers)
+  end
+  
+  def connectToExistingGameServer(client)
+    client.puts "ok" # proceed
+    gameId = client.gets.chomp
+    
+    # ensure we are only trying to check out one game at a time
+    @checkOutSemaphore.synchronize {
+      # get the requested game
+      game = @db.get(:games, gameId)
+      if !game # Invalid gameId
+        return "invalid"
+      end
+
+      # check if game is currently checked out to a server
+      serverAddress = game[:checkedOutTo]
+      # send there
+      return serverAddress if serverAddress
+
+      #else check it out to a new server
+      address = selectServer(@gameServers)
+      game[:checkedOutTo] = address
+      @db.update(:games, game)
+      return address
+    }
   end
   
   # finds the server address of the server with the lowest load
