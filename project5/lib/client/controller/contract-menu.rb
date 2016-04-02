948b8d431c9512378f560350ad6220d9e714b3fd
lindsayl <livojevi@ualberta.ca>
2016-04-02 02:03:03 -0600

    Further simplification.

--- project5/lib/client/controller/contract-menu.rb
+++ project5/lib/client/controller/contract-menu.rb
@@ -7,26 +7,11 @@ module MenuControllerContract
   include Test::Unit::Assertions
   
   def class_invariant
-    assert @implementation, "implementation must exist"
-    methods = [
-      { :name => :players, :args => 0 },
-      { :name => :players=, :args => 1 },
-      { :name => :victoryType, :args => 0 },
-      { :name => :victoryType=, :args => 1 },
-      { :name => :boardControllerType, :args => 0 },
-      { :name => :boardControllerType=, :args => 1 },
-      { :name => :rows, :args => 0 },
-      { :name => :rows=, :args => 1 },
-      { :name => :cols, :args => 0 },
-      { :name => :cols=, :args => 1 }
-    ]
-    methods.each do |method|
-      assert @implementation.method(method[:name]).arity == method[:args], "implementation does not have '#{method[:name]}' method with #{method[:args]} args"
-    end
+    assert @settings.class = SettingsModel, "settings must be a SettingsModel"
   end
   
-  def pre_initialize(type)
-    assert type.class == Symbol && [:menuControllerDefault].include?(type), "menuController type must be a Symbol in [:menuControllerDefault]"
+  def pre_initialize(settings)
+    assert settings.class = SettingsModel, "settings must be a SettingsModel"
   end
 
   def post_initialize
@@ -38,32 +23,5 @@ module MenuControllerContract
   def post_getBoardController(result)
     assert result.class == BoardController, "boardController type must be a BoardController"
   end
-  
-  def pre_settings
-  end
-  
-  def post_settings(result)
-    assert result.class == SettingsModel, "settings type must be a SettingsModel"
-  end
-  
-  def pre_players=(val)
-    assert val.class == Fixnum && val.between?(1,2), "players must be a Fixnum in 1-2"
-  end
-
-  def pre_victoryType=(val)
-    assert val.class == Symbol && [:victoryNormal, :victoryOtto].include?(val), "victoryType must be a Symbol in [:victoryNormal, :victoryOtto]"
-  end
-  
-  def pre_boardControllerType=(val)
-    assert val.class == Fixnum && val.between?(0,1), "boardControllerType must be a Fixnum in 0-1"
-  end
-  
-  def pre_rows=(val)
-    assert val.class == Fixnum && val > 0, "rows must be a Fixnum greater than 0"
-  end
-  
-  def pre_cols=(val)
-    assert val.class == Fixnum && val > 0, "cols must be a Fixnum greater than 0"
-  end
-  
+    
 end
--- project5/lib/client/controller/menu.rb
+++ project5/lib/client/controller/menu.rb
@@ -1,10 +1,11 @@
 require_relative './contract-menu'
-require_relative './menu-default'
 require_relative './board'
 
 # menu/settings controller interface
 class MenuController
   include MenuControllerContract
+  
+  attr_accessor :settings
   
   #initializes the menu controller
   def initialize(settings = SettingsModel.new)
@@ -26,43 +27,6 @@ class MenuController
     post_getBoardController(result)
     class_invariant
     return result
-  end
-  
-  def settings
-    pre_settings
-    
-    result = @settings
-    
-    post_settings(result)
-    class_invariant
-    return result
-  end
-  
-  $settings = [
-    :players, # players - number of players
-    :victoryType, # victoryType - number: 0 = normal, 1 = OTTO
-    :boardControllerType, # the board controller type: 0 = local
-    :rows, # rows - number of rows in board
-    :cols # cols - number of cols in board
-  ]  
-
-  # Creates a getter and setter for each setting
-  $settings.each do |setting|
-    
-    #create getter
-    define_method("#{setting}") do |*args, &block|
-      result = @settings.method("#{setting}").call
-      class_invariant
-      return result
-    end
-    
-    #create setter
-    define_method("#{setting}=") do |*args, &block|
-      val = args[0]
-      @settings.method("#{setting}=").call(val)
-      class_invariant
-    end
-    
   end
   
 end
